0e473fd76b5f7261b841aefb2373c2f2b6f00247
Ricky Yang <grandruiqi@hotmail.com>
2017-09-20 15:46:58 +0800

    Add sort folder

---
+++ sort/single_element_in_a_sorted_array.py
@@ -0,0 +1,19 @@
+'''Given a sorted array consisting of only integers where every element
+   appears twice except for one element which appears once.
+   Find this single element that appears only once.
+   Note: Your solution should run in O(log n) time and O(1) space.'''
+
+def singleNonDuplicate(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    for i in range(0, len(nums), 2):
+        if i == len(nums) - 1: #Take care of 'list index out of range' error
+            return nums[i]
+        if nums[i] != nums[i+1]:
+            return nums[i]
+
+print (singleNonDuplicate([1,1,2]))
+print (singleNonDuplicate([1,1,2,3,3,4,4,8,8]))
+print (singleNonDuplicate([3,3,7,7,10,11,11]))
