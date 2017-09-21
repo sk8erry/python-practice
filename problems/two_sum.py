9dbaa7a9c6c7f1020d7ca181077f8f7a39c04e8d
Ricky Yang <grandruiqi@hotmail.com>
2017-09-21 16:38:03 +0800

    Add two_sum.py (O(n) time complexity)

---
+++ problems/two_sum.py
@@ -0,0 +1,25 @@
+'''Given an array of integers, return indices of the two numbers
+   such that they add up to a specific target.
+   You may assume that each input would have exactly one solution,
+   and you may not use the same element twice.
+   Example:
+   Given nums = [2, 7, 11, 15], target = 9,
+   Because nums[0] + nums[1] = 2 + 7 = 9,
+   return [0, 1]. '''
+
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    buffer_list = [''] * len(nums)
+    for i in range(len(nums)):
+        if (len(nums) == 0) or (len(nums) == 1):
+            return None
+        if target - nums[i] in buffer_list:
+            return i, buffer_list.index(target - nums[i])
+        else:
+            buffer_list[i] = nums[i]
+
+print(twoSum([1,2,3,4],6))
