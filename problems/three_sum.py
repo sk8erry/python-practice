33a2dd05a81d331a048b84e414cae363ac9e3b10
Ricky Yang <grandruiqi@hotmail.com>
2017-09-22 23:32:33 +0800

    just in case

--- problems/three_sum.py
+++ problems/three_sum.py
@@ -3,24 +3,21 @@ def threeSum(nums, target):
     :type nums: List[int]
     :rtype: List[List[int]]
     """
-    buffer_list0 = [''] * len(nums)
     buffer_list = [''] * len(nums)
     ansbuffer = []
     ans = []
     for i in range(len(nums)):
-        buffer_list0[i] = target - nums[i] #Create buffer list0
+        buffer_list[i] = target - nums[i] #Create buffer list0
     for j in range(len(nums)):
         for k in range(len(nums)-1):
             running_list = nums[:k] + nums[k+1:]
             if (len(running_list) == 0):
                 return None
-            if (nums[j] + running_list[k] in buffer_list0):
+            if (nums[j] + running_list[k] in buffer_list):
                 if (nums.index(running_list[k]) != j) and \
                 (nums.index(running_list[k]) != nums.index(target - nums[j] - running_list[k])) \
                 and (j != nums.index(target - nums[j] - running_list[k])):
                     ansbuffer.append([nums[j], running_list[k], target - nums[j] - running_list[k]])
-            else:
-                buffer_list[k] = running_list[k]
     ansbuffer = sorted(ansbuffer)
     if len(ansbuffer) == 1: return ansbuffer
     else:
@@ -34,3 +31,4 @@ def threeSum(nums, target):
 print('answer is ', threeSum([3,-2,1,0],0))
 print('answer is ', threeSum([6,4,7,3],17))
 print('answer is ', threeSum([-2,1,1,3],0))
+print('answer is ', threeSum([0,0,0],0))
