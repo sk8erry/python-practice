d6b2b1f2113f0a84a034f4fb14da931c86e03f21
Ricky Yang <grandruiqi@hotmail.com>
2017-09-26 15:56:19 +0800

    Need to add problemn description

--- problems/magical_string.py
+++ problems/magical_string.py
@@ -1,10 +1,13 @@
 S = "1221121221221121122"
 
-magical_string = [1,2,2]
-
-for i in range(2,15):
-    if i % 2 == 0:
-        magical_string += [1] * magical_string[i]
-    else:
-        magical_string += [2] * magical_string[i]
-print(magical_string)
+def count1sInMagicalString(n):
+    magical_string = [1,2,2]
+    counter = 0
+    if n == 0: return 0
+    for i in range(2, n):
+        if i % 2 == 0:
+            magical_string += [1] * magical_string[i]
+        else:
+            magical_string += [2] * magical_string[i]
+        if magical_string[i] == 1: counter += 1
+    return counter + 1
