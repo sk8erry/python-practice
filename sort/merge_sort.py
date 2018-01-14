15201a84ba51d087047185732146298567a346d6
Ricky Yang <grandruiqi@hotmail.com>
2018-01-14 11:11:29 +0800

    Fix merge_sort.py

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -3,15 +3,38 @@ from random import *
 test_case = []
 for k in range(0,9):
     test_case.append(randint(0,99))
+print (test_case)
 
-def sort(array):
-    for j in range(1,len(array)):
-        key = array[j]
-        i = j
-        while array[i-1] > key and i > 0:
-            array[i] = array[i-1]
-            i = i - 1
-        array[i] = key
+def merge_sort(array):
+    if len(array) > 1:
+        mid = len(array)//2
+        left = array[:mid]
+        right = array[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i = i + 1
+            else:
+                array[k] = right[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(left):
+            array[k] = left[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(right):
+            array[k] = right[j]
+            j = j + 1
+            k = k + 1
+
     return array
 
-print (sort(test_case))
+print (merge_sort(test_case))
