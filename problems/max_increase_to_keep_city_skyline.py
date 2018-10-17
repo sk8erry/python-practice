937a3f3bb424cff9100eef99f08fc56d4a2a8994
Ricky Yang <grandruiqi@hotmail.com>
2018-10-17 12:09:50 -0700

    max_increase_to_kepp_city_skyline.py done

--- problems/max_increase_to_keep_city_skyline.py
+++ problems/max_increase_to_keep_city_skyline.py
@@ -27,19 +27,43 @@ gridNew = [ [8, 4, 8, 7],
 '''
 
 def solution(grid):
+    if len(grid) == 1: return grid[0][0]
     maxIncrease = 0
     maxX = []
     maxY = []
-    for element in grid:
-        maxY.append(max(elment))
-    print(maxY)
+    indexX = 0
+    indexY = 0
+    for row in grid:
+        maxY.append(max(row))
+    for col in zip(*grid[::-1]):
+        maxX.append(max(col))
+    while indexX < len(col):
+        while indexY < len(row):
+            maxIncrease += (min(maxX[indexX], maxY[indexY]) - grid[indexX][indexY])
+            indexY += 1
+        indexX += 1
+        indexY = 0
     return maxIncrease
 
-grid =  [ 
-        [8, 4, 8, 7],
-        [7, 4, 7, 7],
-        [9, 4, 8, 7],
-        [3, 3, 3, 3] 
-        ]
+def getMaxX(grid):
+    maxX = []
+    bufferList = []
+    maxXGenCounter = 0
+    while maxXGenCounter < len(grid):
+        for row in grid:
+            bufferList.append(row[maxXGenCounter])
+            print (bufferList)
+        maxXGenCounter += 1
+        maxX.append(max(bufferList))
+        bufferList = []
+    print (maxX)
+
+grid = [
+    [3, 0, 8, 4], 
+    [2, 4, 5, 7],
+    [9, 2, 6, 3],
+    [0, 3, 1, 0]
+]
 
 print(solution(grid))
+print(getMaxX(grid))
